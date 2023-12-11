b02f0a4c372438e4564c8da2fe8df8f3534d2126
TadasJ <tadasjakovlevas@gmail.com>
2023-12-11 10:23:55 +0200

    task 6 done

--- Task 6/script.js
+++ Task 6/script.js
@@ -7,6 +7,8 @@ atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.
 2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
 -------------------------------------------------------------------------- */
+'use strict';
+console.log('script.js file was loaded');
 
 const users = [
   { id: '1', name: 'John Smith', age: 20 },
@@ -19,3 +21,31 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAvarageAge(arr) {
+  let sum = 0;
+  arr.forEach((userObj) => {
+    const age = userObj.age;
+    // console.log(age);
+    sum += age;
+    return sum;
+  });
+  // console.log(sum);
+  const avgAge = sum / arr.length;
+  // console.log('avgAge ===', avgAge);
+}
+getUserAvarageAge(users);
+
+function getUsersNames(arr) {
+  const usersArrList = [];
+
+  arr.forEach((userObj) => {
+    const usersName = userObj.name;
+    // console.log('usersName ===', usersName);
+    usersArrList.push(usersName);
+    return usersName;
+  });
+  console.log('usersArrList ===', usersArrList);
+}
+
+getUsersNames(users);
