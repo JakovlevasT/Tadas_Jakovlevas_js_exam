950a37d5001307de059971760bb319beb6e7efa0
TadasJ <tadasjakovlevas@gmail.com>
2023-12-11 10:26:48 +0200

    7 done

--- Task 7/script.js
+++ Task 7/script.js
@@ -4,9 +4,18 @@ Turimas "audi" objektas.
 Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
+'use strict';
+console.log('script.js file was loaded');
+
 const audi = {
   make: 'audi',
   model: 'A6',
   year: 2005,
   color: 'white',
 };
+
+function showObjectKeys(obj) {
+  const ats = Object.keys(obj);
+  // console.log('ats ===', ats);
+}
+showObjectKeys(audi);
